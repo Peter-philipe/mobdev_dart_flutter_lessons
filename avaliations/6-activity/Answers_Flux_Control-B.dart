1ad96c010c8c181f13bf2e0d45d67486983526fd
Pedro Felipe <69700651+Peter-philipe@users.noreply.github.com>
2023-04-19 11:47:50 -0300

    Add files via upload

--- avaliations/6-activity/Answers_Flux_Control-B.dart
+++ avaliations/6-activity/Answers_Flux_Control-B.dart
@@ -1,10 +1,52 @@
 
-import 'package:flutter_test/flutter_test.dart';
+
 
 void main(List<String> args) {
   
   print('01) O que é Fluxo de Controle em Dart?');
-  print('');
+  print('Fluxo de controle é usado para contralar a execução de um programa decidindo o que será\n'
+  'ou não usado executado a depender da situação\n');
+
+  print('02) Quais são as três estruturas básicas de Fluxo de Controle em Dart?');
+  print('São elas, estruturas de condicionais, estruturas de loop e estruturas de exceção\n');
+
+
+  print('03) O que é uma instrução if em Dart?');
+  print('Uma instrução que indica que um pedaço de código será executado se um determinada condição for\n'
+  'for satisfeita\n');
+
+  print('04) O que é uma instrução if-else em Dart?');
+  print('Testa uma condição e caso ela for verdadeira um pedaço de código será executado\n'
+  'caso essa condição não seja verdadeira outro pedaço de código será executado\n');
+  
+  print('05) O que é uma instrução switch em Dart?');
+  print('Testa varias condições ao mesmo tempo e executa o código ta que for satisfeita\n');
+
+  print('06) Como usar a instrução switch em Dart?');
+
+  var tipo_de_pilha = "A";
+
+  switch (tipo_de_pilha){
+
+    case "A":
+      print('Pense numa pilha grande\n');
+      break;
+
+    case "AA":
+      print('Pilha média\n');
+      break;
+
+    case "AAA":
+      print('Pilha palito\n');
+      break;
+
+  }
+
+
+  
+
+
+
 
 
 }
